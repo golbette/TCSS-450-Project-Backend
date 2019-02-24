02c67f37eccc662506a738a8cfaece5cc3b48a12
mauricechiu7 <morisbroderick@gmail.com>
2019-02-23 23:05:34 -0800

    fixed pushy services

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.use(bodyParser.json());
 app.use('/register', require('./routes/register.js'));
 app.use('/login', require('./routes/login.js'));
 app.use('/verify', require('./routes/verify.js'));
+app.use('/pushy', require('./routes/pushy.js'));
 app.use('/messaging', require('./routes/messaging.js'));
+
+// app.use('/pushy', middleware.checkToken, require('./routes/pushy.js'));
+// app.use('/messaging', middleware.checkToken, require('./routes/messaging.js'));
 
 
 /** Use middle ware after merged with master for testing */
