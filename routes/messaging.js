e393347afc7b3d32516c494e7d92c915ea071439
Sam Anderson <golbleen@gmail.com>
2019-02-24 15:06:02 -0800

    Update messaging.js

--- routes/messaging.js
+++ routes/messaging.js
@@ -69,7 +69,7 @@ router.post('/create', (req, res) => {
             let checkContacts = 'SELECT verified FROM CONTACTS WHERE (memberid_a=$1 AND memberid_b=$2) OR (memberid_a=$2 AND memberid_b=$1)';
 
             if (i != initiatingUser){
-            db.one(checkContacts, [initiatingUser, i]).then( rows => {
+            db.one(checkContacts, [initiatingUser,  users[i]]).then( rows => {
                 let verified = rows[verified];
 
                 if (verified != 1) {
