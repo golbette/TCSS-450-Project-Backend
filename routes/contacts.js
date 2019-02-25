2ad2876e53f873eed85f5c254178af0e4a5751f3
mauricechiu7 <morisbroderick@gmail.com>
2019-02-24 21:44:03 -0800

    added several contact endpoints.

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,19 +5,84 @@ const bodyParser = require("body-parser");
 router.use(bodyParser.json());
 let msg_functions = require('../utilities/utils.js').messaging;
 
+router.post('/searchcontacts', (req, res) => {
+    let username = req.body['username'];
+    db.one('select firstname, lastname, memberid from members where username = $1', [username]).then(row => {
+        res.send({
+            success:true,
+            message:row
+        })
+    })
+})
+
+router.post('/getcontacts', (req, res) => {
+    let email = req.body['email'];
+    db.one('select memberid from members where email=$1', [email]).then(row => {
+        db.any('select members.firstname, members.lastname, members.email from members where members.memberid = any (select memberid_b from contacts where memberid_a=$1)', [row.memberid]).then(rows => {
+            res.send({
+                success:true,
+                message:rows
+            })
+        }).catch(err => {
+            res.send({
+                success:false,
+                error:err.message,
+                email:email,
+                message:"You don't have any contacts. Search and add people you know!"
+            })
+        })
+    }).catch(err => {
+        res.send({
+            success:false,
+            error:err.message,
+            email:email,
+            message:"This member does not exist or does not have an ID."
+        })
+    })
+})
+
+router.post('/getconnreq', (req, res) => {
+    let email = req.body['email'];
+    db.one('select memberid from members where email = $1', [email]).then(row=>{
+        db.any('select firstname, lastname, memberid from members where memberid = any (select memberid_a from contacts where memberid_b=$1)', [row.memberid]).then(rows=>{
+            res.send({
+                success:true,
+                message:rows
+            })
+        }).catch(err => {
+            res.send({
+                success:false, 
+                message:'You are all caught up!'
+            })
+        })
+    }).catch(err=>{
+        res.send({
+            success:false,
+            message:'Member DNE'
+        })
+    })
+})
+
 router.post('/request',  (req, res) => {
-    let sender = req.body['MemberID_A'];
+    let email = req.body['email'];
     let receiver = req.body['MemberID_B'];
-    let insert = 'INSERT INTO Contacts(MemberID_A, MemberID_B, verified) VALUES ($1, $2, 0)';
-    let select = 'SELECT * FROM Contacts WHERE MemberID_A = $1 AND MemberID_B = $2';
-    let success = true;
+    db.one('select memberid from members where email = $1', [email]).then(row=>{
+        let sender = row.memberid;
+        let insert = 'INSERT INTO Contacts(MemberID_A, MemberID_B, verified) VALUES ($1, $2, 0)';
+        let select = 'SELECT * FROM Contacts WHERE MemberID_A = $1 AND MemberID_B = $2';
+        let success = true;
 
-
-    db.none(select, [sender, receiver]).then(() =>{
-        db.none(insert, [sender, receiver]).then (() => {
-            db.one(select, [sender, receiver]).then(() => {
-                res.send({
-                    success:true
+        db.none(select, [sender, receiver]).then(() =>{
+            db.none(insert, [sender, receiver]).then (() => {
+                db.one(select, [sender, receiver]).then(() => {
+                    res.send({
+                        success:true
+                    })
+                }).catch(err => {
+                    res.send({
+                        success:false,
+                        error:err.message
+                    })
                 })
             }).catch(err => {
                 res.send({
@@ -34,10 +99,9 @@ router.post('/request',  (req, res) => {
     }).catch(err => {
         res.send({
             success:false,
-            error:err.message
+            message:'Member does not exist or member id is missing.'
         })
     })
-
     
 })
 
