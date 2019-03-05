5392174ff464b76862f0f79709490f2d02adecaf
Sam Anderson <golbleen@gmail.com>
2019-03-04 22:54:39 -0800

    remove list endpoint, refactor connreq and connapprove endpoints to accept emails

--- routes/contacts.js
+++ routes/contacts.js
@@ -33,6 +33,8 @@ router.post('/getusername', (req, res) => {
 router.post('/getcontacts', (req, res) => {
     let email = req.body['email'];
     db.one('select memberid from members where email=$1', [email]).then(row => {
+
+
         db.any('select members.firstname, members.lastname, members.email, members.username from members where members.memberid = any (select memberid_b from contacts where memberid_a=$1 and verified = 1)', [row.memberid]).then(rows => {
             res.send({
                 success:true,
@@ -78,39 +80,43 @@ router.post('/getconnreq', (req, res) => {
     })
 })
 
-router.post('/request',  (req, res) => {
-    let email = req.body['email'];
-    let receiver = req.body['MemberID_B'];
+router.post('/connReq',  (req, res) => {
+    let email = req.body['email_a'];
+    let receiver = req.body['email_b'];
     db.one('select memberid from members where email = $1', [email]).then(row=>{
         let sender = row.memberid;
-        let insert = 'INSERT INTO Contacts(MemberID_A, MemberID_B, verified) VALUES ($1, $2, 0)';
-        let select = 'SELECT * FROM Contacts WHERE MemberID_A = $1 AND MemberID_B = $2';
-        let success = true;
 
-        db.none(select, [sender, receiver]).then(() =>{
-            db.none(insert, [sender, receiver]).then (() => {
-                db.one(select, [sender, receiver]).then(() => {
-                    res.send({
-                        success:true
+        db.one('select memberid from members where email = $1', [receiver]).then(row=>{
+            let receiver = row.memberid;
+            let insert = 'INSERT INTO Contacts(MemberID_A, MemberID_B, verified) VALUES ($1, $2, 0)';
+            let select = 'SELECT * FROM Contacts WHERE MemberID_A = $1 AND MemberID_B = $2';
+            let success = true;
+
+            db.none(select, [sender, receiver]).then(() =>{
+                db.none(insert, [sender, receiver]).then (() => {
+                    db.one(select, [sender, receiver]).then(() => {
+                        res.send({
+                            success:true
+                        })
+                    }).catch(err => {
+                        res.send({
+                            success:false,
+                            error:err.message,
+                            errorTime: 'creating addition'
+                        })
                     })
                 }).catch(err => {
                     res.send({
                         success:false,
-                        error:err.message,
-                        errorTime: 'creating addition'
+                        error:err.message
                     })
                 })
             }).catch(err => {
                 res.send({
                     success:false,
-                    error:err.message
+                    error:err.message,
+                    errorTime: 'get receiver'
                 })
-            })
-        }).catch(err => {
-            res.send({
-                success:false,
-                error:err.message,
-                errorTime: 'get receiver'
             })
         })
     }).catch(err => {
@@ -123,82 +129,13 @@ router.post('/request',  (req, res) => {
     })
 })
 
-router.get('/list', (req, res) => {
-    let contactQuery = 'SELECT memberid_a, memberid_b FROM CONTACTS WHERE (MemberID_A = $1 OR MemberID_B = $1) AND verified = 1';
-    let user = req.body['Username'];
-    var contactInfos = [];
-    var contactIDs = [];
-    let getUserID = 'SELECT memberID FROM members WHERE username = $1'
 
-    db.one(getUserID, [user]).then(row => {
-
-        let user = row['memberid']
-        db.manyOrNone(contactQuery, [user]).then(rows => {
-            let members_a = rows['memberid_a'];
-            let members_b = rows['memberid_b'];
-            
-            console.log(members_a);
-            console.log(members_b);
-            console.log(rows);
-
-            let userNameQuery = 'SELECT username FROM members WHERE memberID = $1';
-
-            for (i in members_a){
-                if (members_a[i] != user) {
-                    contactIDs.push(members_a[i]);
-                }
-            }
-
-            for (i in members_b){
-                if (members_b[i] != user) {
-                    contactIDs.push(members_b[i]);
-                }
-            }
-
-            for (i in contactIDs) {
-                //by definition there should only be one user per contact id
-                db.one(userNameQuery, [contactIDs[i]]).then(row => {
-                    name = row['username'];
-                    contactInfos.push(name);
-                }).catch( err => {
-                    res.send({
-                        success:false,
-                        error: err.message
-                    })
-                })
-            }
-        }).catch( err => {
-            res.send({
-                success:false,
-                error: err.message
-            })
-        })
-
-        res.send({
-            success:true,
-            usernames: contactInfos,
-            userids: contactIDs,
-            requester: user,
-        })
-
-    }).catch(err => {
-        res.send({
-            success:false,
-            error:err.message
-        })
-    })
-
-        
-
-})
-
-
-router.post('/approve', (req, res) => {
-    let sender = req.body['Username_A'];
-    let receiver = req.body['Username_B'];
+router.post('/connApprove', (req, res) => {
+    let sender = req.body['email_a'];
+    let receiver = req.body['email_b'];
 
     let select = 'SELECT verified FROM CONTACTS WHERE MemberID_B = $1 AND MemberID_A = $2';
-    let getUserName = 'SELECT memberID FROM members WHERE username = $1'
+    let getUserName = 'SELECT memberID FROM members WHERE email = $1'
 
     db.one(getUserName, [sender]).then( rows =>{
         sender = rows['memberid']
