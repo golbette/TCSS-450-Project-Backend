93b8bfefcd8c7396fd13e681a4fc9ef25592f771
Sam Anderson <golbleen@gmail.com>
2019-03-05 21:28:17 -0800

    Update contacts.js

--- routes/contacts.js
+++ routes/contacts.js
@@ -241,7 +241,7 @@ router.post('/connApprove', (req, res) => {
 
 router.post('/connSent', (req, res) => {
     let sender = req.body['email'];
-    let select = 'SELECT verified FROM CONTACTS WHERE (MemberID_B = $1 OR MemberID_A = $1) AND verified = false';
+    let select = 'SELECT verified FROM CONTACTS WHERE (MemberID_B = $1 OR MemberID_A = $1) AND verified = 0';
     let getUserID = 'SELECT memberID FROM members WHERE email = $1'
 
     db.one(getUserID, [sender]).then(row => {
@@ -256,12 +256,12 @@ router.post('/connSent', (req, res) => {
                     result: rows});
             }
         }).catch(err => {
-            res.send({success: true,
+            res.send({success: false,
                 error: err.message,
                 time:"Retrieving contacts"});
         })
     }).catch(err => {
-        res.send({success: true,
+        res.send({success: false,
             error: err.message,
             time:"Getting UserID"});
     })
